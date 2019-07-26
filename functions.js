485278a583898dca0c9273fc894622bb32ad977b
Henrique Alho <henriquejvalho@gmail.com>
2019-07-26 15:31:06 +0100

    Datatables server-side; ElementHeightChange event

--- functions.js
+++ functions.js
@@ -10,3 +10,73 @@ if (!match) { return 0; }
             // Adjust for scientific notation.
             - (match[2] ? +match[2] : 0));
 }
+
+/**
+ * onElementHeightChange event
+ * Credits: https://stackoverflow.com/a/14901150/2282107 / vsync
+ */
+function onElementHeightChange(elm, callback) {
+    var lastHeight = elm.clientHeight, newHeight;
+    (function run() {
+        newHeight = elm.clientHeight;
+        if (lastHeight != newHeight)
+            callback();
+        lastHeight = newHeight;
+
+        if (elm.onElementHeightChangeTimer)
+            clearTimeout(elm.onElementHeightChangeTimer);
+
+        elm.onElementHeightChangeTimer = setTimeout(run, 200);
+    })();
+}
+// Usage: 
+onElementHeightChange(document.body, function () {
+    console.log('I changed height!');
+});
+
+/**
+ * Returns a file extension
+ */
+function getFileExtension(filename) {
+    filename = filename.toLowerCase();
+    return filename.split('.').pop();
+}
+
+/**
+ * Working datatables with server-side processing
+ * @Datatables version: 1.10.5
+ */
+/*$(document).ready(function () {    
+    //#region Create datatble
+    $('#table').DataTable({
+        "bInfo": false,
+        "searching": false,
+        "bLengthChange": false,
+        "paging": true,
+        'iDisplayLength': 5,
+        columns: [
+            { 'data': 'CustomerNumber' },
+            { 'data': 'CustomerName' },
+            { 'data': 'CustomerType' },
+        ],
+        "bServerSide": true,
+        "bProcessing": true,
+        "ordering": false,
+        sAjaxSource: 'CustomerHandler.ashx?messageId=10', // Change the handler URL 
+        sServerMethod: 'POST',
+        oLanguage: { // Translate strings
+            sProcessing: '<img src="loading.gif">', // Change processing img / text
+            oPaginate: { sPrevious: 'Previous page', sNext: 'Next page' },
+            sEmptyTable : 'No results found',
+            sInfoEmpty: 'Empty table',
+            sZeroRecords: 'No records'
+        },
+        fnInitComplete : function() {
+            // Hide pagination when no results
+            if ($(this).find('tbody tr').length == 0) {
+                   $('.dataTables_paginate').hide();
+            }
+        }
+    });
+});*/
+
